2f163c8a7c012de2efe234fd0f582ee813e8a853
48774861 <112006435+48774861@users.noreply.github.com>
2023-11-27 16:09:12 -0600

    Start of Program

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/marcpham/CLionProjects/CS3353/program4_cs3353  /hw4.cpp" "CMakeFiles/main.dir/hw4.cpp.o" "gcc" "CMakeFiles/main.dir/hw4.cpp.o.d"
+  "/Users/marcpham/CLionProjects/CS3353/program4_cs3353  /hw4test.cpp" "CMakeFiles/main.dir/hw4test.cpp.o" "gcc" "CMakeFiles/main.dir/hw4test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/hw4.cpp.o"
+  "CMakeFiles/main.dir/hw4.cpp.o.d"
+  "CMakeFiles/main.dir/hw4test.cpp.o"
+  "CMakeFiles/main.dir/hw4test.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
